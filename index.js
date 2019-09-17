d771977bd4b397afe43e78ec3e1d8156ecb40f44
BK(김병규) <wantutopia@gmail.com>
2019-09-17 11:12:47 +0900

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function init(){
     var days = 0;
     var hour = 0;
 
-    for (var i=1; i<=31; i++) {
+    for (var i=0; i<=31; i++) {
         var tmp_day = i < 10 ? "0" + i.toString() : i;
         var tmp_month = month <10 ? "0" + month.toString() : month;
         $.get( "https://ezkhuman.bizmeka.com/product/golvwkmng/onedaygolvwkmngpers/readOnedayGolvwkMngPers.do?viewMenuCode=PJ02&enplcCd=G001&workDate=" 
@@ -14,7 +14,8 @@ function init(){
             + tmp_month + "." 
             + tmp_day)
         .done(function(response) {
-            if(response.workHour !== "00:00") {
+            console.log(response);
+            if(response.workHour !== "00:00" && response.workHour) {
                 days++;
                 hour += Number(response.workHour.split(":")[0])
                 hour += Number(response.workHour.split(":")[1]) / 60
@@ -24,10 +25,17 @@ function init(){
                 $("#days").text(days + "일");
                 $("#total_hour").text(hour.toFixed(2) + "시간");
                 $("#mid_hour").text((hour/days).toFixed(2) + "시간");
-          }
+             }
+
+            if (i > 0 && !response.workHour) {
+                document.body.innerHTML = "KT 비즈메카 근태관리에서 로그인 해주세요. (<a href='https://ezkhuman.bizmeka.com'>https://ezkhuman.bizmeka.com</a>) "
+                    + "<br><br>비즈메카의 근태관리 메뉴 클릭 후 이용하실 수 있습니다.";
+                return;
+            }
         })
         .fail(function() {
-           document.body.innerText = "KT 비즈메카에서 로그인 해주세요.";
+            document.body.innerHTML = "KT 비즈메카 근태관리에서 로그인 해주세요. (<a href='https://ezkhuman.bizmeka.com'>https://ezkhuman.bizmeka.com</a>) "
+                + "<br><br>비즈메카의 근태관리 메뉴 클릭 후 이용하실 수 있습니다.";
            return;
          });         
     }
